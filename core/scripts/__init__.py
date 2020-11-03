379ca9e131078e99ed6e5e6dabfd77aa83daa6ec
Arif Dzikrullah <arifdzikrullah@Arifs-MacBook-Pro.local>
2020-11-03 15:40:08 +0700

    added import_bank

--- core/scripts/__init__.py
+++ core/scripts/__init__.py
@@ -21,3 +21,25 @@ from core.scripts.administrative import *
 # from core.scripts.numbering import *
 # import_provices()
 # import_regencies()
+
+
+def import_bank():
+    from enterprise.libs.pay_constants import BANK_CHOICES
+    from enterprise.structures.authentication.models import User
+    from slugify import slugify
+
+    user, created = User.objects.get_or_create(
+        full_name='Admin',
+        email='admin@investx.id',
+        phone_number='6281111'
+    )
+    for b in BANK_CHOICES:
+        display_name = b[1]
+        short_name = slugify(display_name)
+        code = b[0]
+        Bank.objects.get_or_create(
+            display_name=display_name,
+            short_name=short_name,
+            code=code,
+            created_by=user
+        )
